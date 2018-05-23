c17842223fc314e6eb1a6043a53e4e1e0cf10551
Maretzky85 <maretzky85@gmail.com>
2018-05-23 18:14:20 +0200

    added try/except for file open

--- data_manager.py
+++ data_manager.py
@@ -6,9 +6,12 @@ def import_data_from_file(filename):
     Opens file defined in filename
     Returns list of dicts
     '''
-    with open(filename, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        data = []
-        for row in reader:
-            data.append(row)
-    return data
+    try:
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            data = []
+            for row in reader:
+                data.append(row)
+        return data
+    except FileNotFoundError:
+        return None
