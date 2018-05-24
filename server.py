edd727eb864f4f0a4972da93731d8d5c3592f59b
Maretzky85 <maretzky85@gmail.com>
2018-05-24 17:43:33 +0200

    pep8 server.py - it was making me furious ;)

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import logic
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/list")
 def main():
@@ -10,49 +11,53 @@ def main():
     answers = logic.get_all_data("a")
     return render_template('list.html', questions=questions, answers=answers)
 
-@app.route("/new_question", methods = ['GET', 'POST'])
-def new_question(): 
+
+@app.route("/new_question", methods=['GET', 'POST'])
+def new_question():
     return render_template('new_question.html')
 
-@app.route("/question/<question_id>", methods = ['GET', 'POST'])
-def question(question_id): 
+
+@app.route("/question/<question_id>", methods=['GET', 'POST'])
+def question(question_id):
 
     return render_template('question.html')
 
-@app.route("/question/<question_id>/edit", methods = ['GET', 'POST'])
-def edit_question(question_id): 
+
+@app.route("/question/<question_id>/edit", methods=['GET', 'POST'])
+def edit_question(question_id):
 
     return render_template('question.html')
 
-@app.route("/question/<question_id>/delete", methods = ['GET', 'POST'])
-def delete_question(question_id): 
+
+@app.route("/question/<question_id>/delete", methods=['GET', 'POST'])
+def delete_question(question_id):
 
     return render_template('question.html')
 
-@app.route("/question/<question_id>/new-answer", methods = ['GET', 'POST'])
-def add_answer(question_id): 
+
+@app.route("/question/<question_id>/new-answer", methods=['GET', 'POST'])
+def add_answer(question_id):
 
     return render_template('question.html')
 
-@app.route("/answer/<answer_id>/delete", methods = ['GET', 'POST'])
-def delete_answer(question_id): 
+
+@app.route("/answer/<answer_id>/delete", methods=['GET', 'POST'])
+def delete_answer(question_id):
 
     return render_template('list.html')
 
+
 @app.route("/question/<question_id>/vote-up")
-def vote_up(question_id): 
+def vote_up(question_id):
 
     return render_template('question.html')
+
 
 @app.route("/question/<question_id>/vote-down")
-def vote_down(question_id): 
+def vote_down(question_id):
 
     return render_template('question.html')
-
-
-
-
 
 
 if __name__ == '__main__':
-    app.run (debug=True)
+    app.run(debug=True)
