e62581a975b226e166f8729c92f4ebf9046a0e47
Maretzky85 <maretzky85@gmail.com>
2018-05-25 11:40:12 +0200

    layout and list.html - modified for better view, data_manager.py - added import headers and write to csv - tested

--- data_manager.py
+++ data_manager.py
@@ -15,3 +15,29 @@ def import_data_from_file(filename):
         return data
     except FileNotFoundError:
         return None
+
+
+def import_header(filename):
+    ''' 
+    Opens file and import header for csv dictwriter
+    '''
+    with open(filename) as datafile:
+        lines = []
+        for data in datafile:
+            lines.append(data)
+    return lines[0].strip("\n").split(",")
+
+
+def export_data_to_file(filename, data):
+    '''
+    Opens file to write, and writes data as csv file
+    Args:
+    filename - patch and filename
+    data - list of dicts
+    '''
+    fieldnames = import_header(filename)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for line in data:
+            writer.writerow(line)
