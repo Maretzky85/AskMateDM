2a4fad386ac28361c4bde35a7cdbff6811c76dd5
Maretzky85 <maretzky85@gmail.com>
2018-07-11 16:51:00 +0200

    rebase from master

--- data_manager.py
+++ data_manager.py
@@ -189,6 +189,7 @@ def add_user(cursor, name, date):
                 {"name": name, "date": date,})
 
 
+@connection_handler.connection_handler
 def get_answer_by_user(cursor, user_id):
     cursor.execute("""
                 SELECT *
